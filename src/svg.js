9f916fac7ec2becc7cbc82fad6cb5e0af46b78bd
Mohanad Safwat <57914175+MohanadSafwat@users.noreply.github.com>
2022-06-03 06:29:53 +0200

    Create svg.js

---
+++ src/svg.js
@@ -0,0 +1,27 @@
+import { ReactComponent as RainChanceIcon } from "./svg/rain_light_color.svg";
+import { ReactComponent as WindIcon } from "./svg/windy_color.svg";
+import { ReactComponent as Sunrise } from "./svg/sunrise.svg";
+import { ReactComponent as Sunset } from "./svg/sunset.svg";
+import { ReactComponent as Min } from "./svg/min.svg";
+import { ReactComponent as Max } from "./svg/max.svg";
+import { ReactComponent as Back } from "./svg/back.svg";
+import { ReactComponent as AppIcon } from "./svg/appIcon.svg";
+import { ReactComponent as AppIconD } from "./svg/appIconD.svg";
+import { ReactComponent as Sunset2 } from "./svg/sunset2.svg";
+import { ReactComponent as Sunrise1 } from "./svg/sun1.svg";
+
+var Icons = {
+  RainChance: RainChanceIcon,
+  WindIcon: WindIcon,
+  Sunrise: Sunrise,
+  Sunset: Sunset,
+  Min: Min,
+  Max: Max,
+  Back: Back,
+  AppIcon:AppIcon,
+  AppIconD:AppIconD,
+  Sunrise1,
+  Sunset2
+};
+
+export { Icons };
